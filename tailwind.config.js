2572e3f959be9a69c712c6429c98122946c0858c
mandeepSingh <mandeepraj2312@gmail.com>
2024-02-16 11:48:00 +0530

    making the website responsive about.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html"],
+  mode: "jit",
+
+  content: ["./*.html", "./views/*.html", "./*.css"],
   theme: {
     extend: {
       colors: {
-        black: {
+        black: { 
           default: "#000000",
           dark: "#292F36",
           light: "#4D5053",
@@ -19,9 +21,10 @@ export default {
         "DM-serif-Display": ["DM Serif Display", "serif"],
         Inter: ["Inter", "serif"],
         Jost: ["Jost", "sans-serif"],
+        "DM-serif-text": ["DM Serif Text", "serif"],
       },
       fontSize: {
-        "xlp": ["1.375rem", "2.0625rem"],
+        xlp: ["1.375rem", "2.0625rem"],
         "2xlp": ["1.75rem", "2.625rem"],
         "3xlp": ["2.5rem", "3.125rem"],
         "5xlp": ["3.438rem", "4.3125rem"],
@@ -30,7 +33,11 @@ export default {
       },
       backgroundImage: {
         "hero-image": "url('/assets/images/backgrounds/Rectangle 1.png')",
-        "workFrame1": "url('assets/images/backgrounds/Frame 2.png')",
+        workFrame1: "url('assets/images/backgrounds/Frame 2.png')",
+        "About-Us-img": "url('assets/images/backgrounds/Rectangle 11.png')",
+      },
+      screens: {
+        xsm: { min: "320px", max: "359px" },
       },
     },
   },
