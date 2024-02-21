db3608aeda77c76a3cbef9ee332652b75be57919
mandeepSingh <mandeepraj2312@gmail.com>
2024-02-21 14:38:40 +0530

    fix:resolve styling issue in buttonElement.js
    To Resolve button styling issue in buttonElement.js
    - create  class "btn" in style.css
    - utilize "btn" in buttonElement.js
    - hard-code color for createButton() in main.js
    
    Utilize buttonElement.js for index and about page

--- components/buttonElement.mjs
+++ components/buttonElement.mjs
@@ -1,34 +1,11 @@
-export function createButton() {
-  let blackButtons = document.getElementsByClassName("button-black"); 
-  let orangeButtons = document.getElementsByClassName("button-orange");  
-  let color;
-  let classNames = [
-    "flex",
-    "items-center",
-    "rounded-2xl",
-    "px-8",
-    "py-4",
-    "font-Inter",
-    "text-lg",
-    "font-semibold",
-    "text-white",
-    "xl:px-10",
-    "xl:py-6",
-  ]; // Define classNames array with common classes
+export function createButton(buttonClass, bgColorClass, imgUrl) {
+  const buttons = document.getElementsByClassName(buttonClass);
 
 
-  // Loop through each button in the collection
-  Array.from(blackButtons).forEach((button) => {
-    // Add color class
-    button.classList.add("bg-black-dark");
+  Array.from(buttons).forEach((button) => {
+    button.style.backgroundColor = bgColorClass;
+    button.classList.add("btn");
 
-    // Add common classes
-    classNames.forEach((className) => {
-      button.classList.add(className);
-    });
-
-    // Create and append image
-    const imgURL = "../assets/images/icons/arrow.png";
     const img = document.createElement("img");
     img.classList.add(
       "grayscale-100",
@@ -37,31 +14,7 @@ export function createButton() {
       "invert",
       "filter"
     );
-    img.src = imgURL;
+    img.src = imgUrl;
     button.appendChild(img);
   });
-  Array.from(orangeButtons).forEach((button) => {
-    // Add color class
-    button.classList.add("bg-orange-light");
-
-    // Add common classes
-    classNames.forEach((className) => {
-      button.classList.add(className);
-    });
-
-    // Create and append image
-    const imgURL = "../assets/images/icons/arrow.png";
-    const img = document.createElement("img");
-    img.classList.add(
-      "grayscale-100",
-      "ml-3",
-      "brightness-0",
-      "invert",
-      "filter"
-    );
-    img.src = imgURL;
-    button.appendChild(img);
-  });
-
-  
 }
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { createButton } from "./components/buttonElement.mjs";
 import { slider } from "./components/sliderSupport.mjs";
-createButton();
+createButton("button-black", "#292F36", "../assets/images/icons/arrow.png");
+createButton("button-orange", "#CDA274", "../assets/images/icons/arrow.png");
+
 slider();
