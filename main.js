e88507156c8327c0f84fa39255c5ec5fe63da633
mandeepSingh <mandeepraj2312@gmail.com>
2024-02-23 21:30:20 +0530

    feat:create component for header
    Create a function "createHeader" in components/header.mjs .
    Calling createHeader() in main.js
    Utilize header component in all html files

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import { createButton } from "./components/buttonElement.mjs";
-import { slider } from "./components/sliderSupport.mjs";
-import { createArticleContainer } from "./components/articleSection.mjs";
+import { createHeader } from "./components/header.mjs";
 import { createFooter } from "./components/footer.mjs";
+import { slider } from "./components/sliderSupport.mjs";
+import { createButton } from "./components/buttonElement.mjs";
+import { createArticleContainer } from "./components/articleSection.mjs";
 
 createFooter();
+createHeader();
 createButton("button-black", "#292F36", "../assets/images/icons/arrow.png");
 createButton("button-orange", "#CDA274", "../assets/images/icons/arrow.png");
 slider();
