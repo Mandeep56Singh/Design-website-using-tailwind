142ed4cc537ec73a601253666e4b2d10d8a96943
mandeepSingh <mandeepraj2312@gmail.com>
2024-02-23 18:24:56 +0530

    feat:create component for footer

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import { createButton } from "./components/buttonElement.mjs";
 import { slider } from "./components/sliderSupport.mjs";
 import { createArticleContainer } from "./components/articleSection.mjs";
+import { createFooter } from "./components/footer.mjs";
+createFooter;
 createButton("button-black", "#292F36", "../assets/images/icons/arrow.png");
 createButton("button-orange", "#CDA274", "../assets/images/icons/arrow.png");
 slider();
 createArticleContainer();
+createFooter();
