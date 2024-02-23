4779f3ed6260bc6983b3716a61be2b1f894b0480
mandeepSingh <mandeepraj2312@gmail.com>
2024-02-23 16:43:31 +0530

    feat:make js component for article&news section

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { createButton } from "./components/buttonElement.mjs";
 import { slider } from "./components/sliderSupport.mjs";
+import { createArticleContainer } from "./components/articleSection.mjs";
 createButton("button-black", "#292F36", "../assets/images/icons/arrow.png");
 createButton("button-orange", "#CDA274", "../assets/images/icons/arrow.png");
-
 slider();
+createArticleContainer()
