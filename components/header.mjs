f8de9de82b863e31bdfee38100bda5b52e3ed4ff
mandeepSingh <mandeepraj2312@gmail.com>
2024-02-28 22:07:56 +0530

    feat:change text color of header link to orange based on current page

--- components/header.mjs
+++ components/header.mjs
@@ -1,11 +1,21 @@
 export function createHeader() {
   const header = document.querySelector("header");
-  const links = [
+  header.classList.add("xl:w-[1210px]","mx-auto")
+
+ const links = [
     "/index.html",
-   "/views/about.html",
-   "/views/service.html",
-   "/views/projects.html",
+    "/views/service.html",
+    "/views/projects.html",
+    "/views/about.html",
+    "/views/contact.html",
 ];
+const linksText = [
+  "Home",
+  "Services",
+  "Projects",
+  "About",
+  "Contact",
+]
 
   header.innerHTML = `
      <div class="mt-5 flex h-[50px] justify-between sm:mt-4 xsm:mt-2">
@@ -23,19 +33,18 @@ export function createHeader() {
       </div>
 
       <nav class="flex items-center gap-3 xl:gap-12">
-        <ul class="hidden xl:mr-5 xl:flex xl:gap-12">
+        <ul class="  hidden xl:mr-5 xl:flex xl:gap-12">
           <li class="font-Jost text-xl font-medium">
-            <a href="${links[0]}">Home</a>
+            <a href="${links[0]}">${linksText[0]}</a>
           </li>
           <li class="font-Jost text-xl font-medium">
-            <a href="${links[1]}">Pages</a>
+            <a href="${links[1]}">${linksText[1]}</a>
           </li>
           <li class="font-Jost text-xl font-medium">
-            <a href="${links[2]}">Services</a>
+            <a href="${links[2]}">${linksText[2]}</a>
           </li>
-          <li class="font-Jost text-xl font-medium"><a href="${links[3]}">Project</a></li>
-          <li class="font-Jost text-xl font-medium"><a href="#">Blog</a></li>
-          <li class="font-Jost text-xl font-medium"><a href="#">Contact</a></li>
+          <li class="font-Jost text-xl font-medium"><a href="${links[3]}">${linksText[3]}</a></li>
+          <li class="font-Jost text-xl font-medium"><a href="${links[4]}">${linksText[4]}</a></li>
         </ul>
         <div>
           <img
@@ -51,4 +60,19 @@ export function createHeader() {
         />
       </nav>
     </div>`;
+ 
+    const currentPage = window.location.href;
+
+    // Get all the links in the navigation
+    const navLinks = document.querySelectorAll("header ul li a");
+
+    // Loop through each link and check if it matches the current page URL
+    navLinks.forEach((link) => {
+      if (link.href === currentPage) {
+        link.classList.add("text-orange-500"); // Apply orange color to the active link
+      } else {
+        link.classList.remove("text-orange-500"); // Remove orange color from non-active links
+      }
+    });
+
 }
