1778d5a55db53d7da4566000938cd87525474c07
mandeepSingh <mandeepraj2312@gmail.com>
2024-03-03 21:38:59 +0530

    fix:resolve categoryBar not showing after transition to projects page

--- components/categoryBar.js
+++ components/categoryBar.js
@@ -1,50 +1,7 @@
 function createCategoryBar() {
     const projectCategory = document.querySelector('.project-category');
-    const categoriesTitles = [
-        "Bathroom",
-        "Bedroom",
-        "Kitchen",
-        "Living Room",
-    ];
-    const categoryContainer = document.createElement('div');
-    categoryContainer.classList.add(
-      "flex",
-      "border",
-      "border-orange-light",
-      "rounded-xl",
-      "mx-auto",
-      "w-[266px]",
-      "sm:w-[590px]",
-      "lg:w-[834.2px]",
-      "h-[75px]",
-      "overflow-auto",
-      "scroll-none"
-    );
-    for(let i = 0; i < categoriesTitles.length; i++) {
-        const btn = document.createElement('button');
-        btn.classList.add(
-          "btn",
-          "font-Jost",
-          "text-lg",
-          "font-semibold",
-          "text-black-dark",
-          "py-auto",
-          "sm:px-[66px]",
-          "px-[32px]",
-          "rounded-xl",
-          "whitespace-nowrap"
-        );
-        btn.textContent = categoriesTitles[i];
-        if(categoriesTitles[i] === "Bedroom") {
-            btn.classList.add("bg-orange-light", "text-white");
-        }
-        categoryContainer.appendChild(btn);
-    }
    
-
-   
-    projectCategory.appendChild(categoryContainer);
-    const buttonArray = document.querySelectorAll(".btn");
+    const buttonArray = projectCategory.querySelectorAll("button");
     buttonArray.forEach((btn) => {
         btn.addEventListener("click", () => {
              const activeBtn = document.querySelector(".btn.bg-orange-light");
